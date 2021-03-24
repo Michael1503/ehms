4989ceb98ad1cab0905c632dd99d777e31b77aab
Michael Rafael <michael1503@gmail.com>
2021-03-24 11:03:16 -0300

    Nova Home

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,8 @@ import { useState } from 'react'
 function Home(){
     return <div>
                 <h1><center>Home EHMS</center></h1>
-                <Contador />
             </div>
 
-}
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
 }
 
 export default Home
